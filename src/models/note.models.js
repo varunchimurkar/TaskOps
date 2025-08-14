e65ddc02c904bc93450fe4d1f48cee1159a51602
Varun Chimurkar <varunchimurkar0908@gmail.com>
2025-08-14 11:19:31 +0530

    feat(models): add Note schema and model

--- src/models/note.models.js
+++ src/models/note.models.js
@@ -2,7 +2,25 @@ import mongoose, {Schema} from "mongoose";
 
 const noteSchema = new Schema({
 
-})
+    project: {
+        type: Schema.Types.ObjectId,
+        ref: "Project",
+        required : true,
+
+    },
+
+    createdBy : {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+
+    content : {
+        type: String,
+        required: true
+    }
+
+}, {timestamps: true})
 
 
 export const Note = mongoose.model("Note", noteSchema)
